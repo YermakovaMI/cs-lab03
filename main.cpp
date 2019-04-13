714ca38d0bfd8005a0105e4a5fa4e6a55d2d6e22
MIY <you@example.com>
2019-04-13 20:39:58 +0300

    code:изменение функции

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 using namespace std;
 
@@ -13,30 +14,14 @@ input_numbers(size_t count) {
     return result;
 }
 
- void find_minmax (vector<double> numbers, double* min, double* max)
-    {
-        *min = numbers[0];
-        *max = numbers[0];
-        for (double number : numbers)
-        {
-            if (number < *min)
-            {
-                *min = number;
-            }
-            if (number > *max)
-            {
-                *max = number;
-            }
-        }
 
-    }
 
 //функция расчёта количества чисел
 vector<size_t> make_histogram(vector <double> numbers, size_t bin_count )
 {
     double min = numbers[0];
     double max = numbers[0];
-    find_minmax (numbers, &min, &max);
+    find_minmax (numbers, min, max);
     vector<size_t> bins(bin_count);
     for (double number : numbers)
     {
