4d5240bb9c11cc96842e300ff57d6d9370dae5bc
MIY <you@example.com>
2019-04-15 08:28:19 +0300

    code:дополнение задания

--- histogram.cpp
+++ histogram.cpp
@@ -51,16 +51,37 @@ void show_histogram_svg(const vector<size_t>& bins)
     {
         const auto IMAGE_WIDTH = 400;
         const auto IMAGE_HEIGHT = 300;
-        const auto TEXT_LEFT = 20;
+        const auto TEXT_LEFT = 10;
         const auto TEXT_BASELINE = 20;
         const auto TEXT_WIDTH = 50;
         const auto BIN_HEIGHT = 30;
-    svg_begin(400, 300);
+
+         const size_t MAX_ASTERISK = IMAGE_WIDTH - TEXT_LEFT - TEXT_WIDTH;
+
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
+        }
+    }
+    max_count*=10;
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    double scaling_factor=1;
+        if (scaling_needed)
+        {
+            scaling_factor = (double)MAX_ASTERISK / max_count;
+        }
+    max_count*=scaling_factor;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
     for (size_t bin : bins) {
-    const double bin_width = 10 * bin;
-    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
+    const double bin_width = 10 * bin * scaling_factor;
+    svg_rect(max_count+TEXT_LEFT-bin_width, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
+    svg_text(max_count+2*TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+
     top += BIN_HEIGHT;
 }
     svg_end();
